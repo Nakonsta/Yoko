b29c305c8c2e052d6e02f0e72f9fc1871e27a82c
Vertukhov Yaroslav <yvertukhov@extyl-pro.ru>
2020-09-28 13:14:17 +0300

    fix mobile slider

--- src/assets/js/main/index.js
+++ src/assets/js/main/index.js
@@ -1,10 +1,14 @@
+//Полифилы
+import './modules/polyfill.js'
+
 //Отрытие/закрытие мобильного меню
 import './modules/mobile-menu.js'
 
 import './modules/news-detail-frame-fix'
 
 // sliders
-import './modules/sliders';
+import './modules/slider-advantages';
+import './modules/slider-gallery-about'
 import './modules/more-button-menu'
 /**
  * Полифил для object-fit
