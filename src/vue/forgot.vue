98309ab8c89baa2b348e7f2944896f07066df62a
Vertuhkov Yaroslav <yvertukhov@extyl-pro.ru>
2020-11-06 11:01:09 +0300

    fix

--- src/vue/forgot.vue
+++ src/vue/forgot.vue
@@ -4,7 +4,7 @@
             <div v-show="error" v-html="error" class="form__error"></div>
             <ValidationProvider name="E-mail" v-slot="{ errors, failed }" rules="required|email" tag="label" class="field__container">
                 <span class="field__label">Введите E-mail</span>
-                <input :class="{field: true, error: failed}" type="text" name="email" v-model="email">
+                <input :class="{field: true, error: failed}" type="text" name="email" placeholder="forexample@gmail.com" v-model="email">
                 <span v-show="failed" class="field__error">{{ errors[0] }}</span>
             </ValidationProvider>
             <button class="btn">Выслать письмо</button>
