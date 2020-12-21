16bda33eaa980700925b1a8947bc731c1f4dd47d
Vertuhkov Yaroslav <yvertukhov@extyl-pro.ru>
2020-12-21 18:58:39 +0300

    add role router

--- src/vue/admin/pages/accreditations/details.vue
+++ src/vue/admin/pages/accreditations/details.vue
@@ -11,9 +11,10 @@
                     v-if="!viewType.isCreate && accreditation.status"
                     :status="accreditation.status"
                 ></accreditations-steps>
+                <accreditationsMessage v-if="viewType.isCreate" />
                 <accreditations-title
                     :title="`Заявка на аккредитацию ${viewType.isCreate ? '' : `№${id}`}`"
-                    margin="32px 0 0 0"
+                    :margin="!viewType.isCreate && accreditation.status ? '32px 0 0 0' : '0'"
                 ></accreditations-title>
 
                 <div class="divider"></div>
@@ -163,10 +164,12 @@ import AccreditationDetailsSelect from '../../../components/admin/accreditations
 import AccreditationDetailsCheckbox from '../../../components/admin/accreditations/details/AccreditationDetailsCheckbox.vue'
 import AccreditationDetailsFileUploader from '../../../components/admin/accreditations/details/AccreditationDetailsFileUploader.vue'
 import AccreditationDetailsEmpty from '../../../components/admin/accreditations/details/AccreditationDetailsEmpty.vue'
+import accreditationsMessage from '../../../components/blocks/accreditationsMessage'
 
 export default {
     name: 'accreditation-details',
     components: {
+        accreditationsMessage,
         AccreditationsSteps,
         AccreditationsTitle,
         AccreditationDetailsSelect,
@@ -386,9 +389,12 @@ export default {
 @import '../../../../assets/sass/mixins/mq';
 
 .accreditation-details {
-    padding: rem(80px) 0 rem(40px) !important;
+    padding: rem(40px) 0 rem(40px) !important;
     border-radius: 6px;
     background-color: #fff;
+    @media(max-width: 1024px) {
+      padding: rem(80px) 0 rem(40px) !important;
+    }
 
     &__header {
         padding: 0 rem(40px);
