e5cf7fdbc422459f319a6356d6b5716142812648
Vertuhkov Yaroslav <yvertukhov@extyl-pro.ru>
2021-01-11 12:14:12 +0300

    fix

--- src/vue/components/blocks/markSizeCardLarge.vue
+++ src/vue/components/blocks/markSizeCardLarge.vue
@@ -15,7 +15,9 @@
         </div>
         <div v-if="item.company_id" class="table-cell__company cable-info__company" data-name="Компания">
           <span>
-            {{ item.company.name }}
+            <a :href="`/compregister/${item.company.id}`">
+              {{ item.company.name }}
+            </a>
           </span>
           <span class="cable-info__company-desc">
             Рейтинг компании:
@@ -180,9 +182,16 @@ export default {
               &:first-child {
                 display: block;
               }
-            }
-            &-desc {
-              font-weight: 400;
+              > a {
+                transition: .3s color;
+                color: #31ACB8;
+                &:hover {
+                  color: #4cb9c3;
+                }
+              }
+              &-desc {
+                font-weight: 400;
+              }
             }
         }
         &__price {
