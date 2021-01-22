e11ed645fb2e6708717613b93cf88135eb38ee6c
Anastasiia Konstantinovich <na.konsta@gmail.com>
2021-01-22 18:31:27 +0300

    Format date was removed from functions

--- src/vue/helpers/functions.js
+++ src/vue/helpers/functions.js
@@ -376,26 +376,5 @@ export default {
             }
             return result;
         },
-        formatDate(string) {
-            var d = new Date(string),
-                month = '' + (d.getMonth() + 1),
-                day = '' + d.getDate(),
-                year = d.getFullYear(),
-                hour = '' + d.getHours(),
-                minutes = '' + d.getMinutes();
-
-            if (month.length < 2)
-                month = '0' + month;
-            if (day.length < 2)
-                day = '0' + day;
-            if (hour.length < 2)
-                hour = '0' + hour;
-            if (minutes.length < 2)
-                minutes = '0' + minutes;
-
-            var time = ' ' + hour + ':' + minutes;
-
-            return [day, month, year].join('.') + time;
-        },
     }
 }
